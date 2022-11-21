d3fbf91945960dd785f36f0202d6683cce911f3a
Damian Parrino <bucanero@users.noreply.github.com>
2022-11-21 16:53:22 -0300

    markdown

---
+++ markdown.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cd PSP
+for file in *.savepatch
+do
+	../parser ${file} -d > /dev/null
+	echo ${file}
+done
+mv *.md ../docs/PSP/
+
+cd ../PSV
+for file in *.savepatch
+do
+	../parser ${file} -d > /dev/null
+	echo ${file}
+done
+mv *.md ../docs/PSV/
+
+cd ../PS3
+for file in *.ps3savepatch
+do
+	../parser ${file} -d > /dev/null
+	echo ${file}
+done
+mv *.md ../docs/PS3/
+
+cd ../PS4
+for file in *.savepatch
+do
+	../parser ${file} -d > /dev/null
+	echo ${file}
+done
+mv *.md ../docs/PS4/
